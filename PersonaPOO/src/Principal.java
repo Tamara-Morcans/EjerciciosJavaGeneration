ff8d5b0f5fa3dd8d802c29cf4a9e12d3bd71d5f8
Tamara Morcans <78194436+Tamara-Morcans@users.noreply.github.com>
2021-07-16 03:16:28 -0500

    EjerciciosJava

---
+++ PersonaPOO/src/Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona prueba = new Persona();
+		prueba.name = "Tamara";
+		prueba.age = 23;
+		System.out.println(prueba.saludar() );
+		System.out.println(prueba.miEdad() );
+	}
+
+}
